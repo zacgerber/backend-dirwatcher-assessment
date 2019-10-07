f9db862ff989df2f0cf44c0c68278d340d682597
Piero Madar <madarp@pobox.com>
2019-10-07 15:56:24 -0400

    add tests folder

---
+++ tests/test_dirwatcher.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import unittest
+
+__author__ = 'madarp'
+
+
+class TestDirwatcher(unittest.TestCase):
+
+    def test_prog(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
